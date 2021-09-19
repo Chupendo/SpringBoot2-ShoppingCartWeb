b6e68b721aaab1f0fc61d433ff80cd422e119901
Chupendo <andresruizpenuela@gmail.com>
2021-09-19 10:45:06 +0200

    stated project empaty

---
+++ ProjectShoppingCartWeb/src/main/java/com/shoppingcar/web/ProjectShoppingCartWebApplication.java
@@ -0,0 +1,13 @@
+package com.shoppingcar.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectShoppingCartWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectShoppingCartWebApplication.class, args);
+	}
+
+}
---
+++ ProjectShoppingCartWeb/src/test/java/com/shoppingcar/web/ProjectShoppingCartWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingcar.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectShoppingCartWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
